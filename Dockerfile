94622023f8fed4440959ceb636e3f9821b8261bd
jorge <jorgeo.1225@gmail.com>
2022-05-30 19:44:39 -0500

    clone spotify

---
+++ Dockerfile
@@ -0,0 +1,2 @@
+FROM nginx:alpine
+COPY /dist/spotify /usr/share/nginx/html
---
+++ src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { LoginPageComponent } from '@modules/auth/pages/login-page/login-page.component';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http'
+import { CookieService } from 'ngx-cookie-service';
+import { InjectSessionInterceptor } from '@core/interceptors/inject-session.interceptor';
+
+
+
+
+@NgModule({
+  declarations: [ //TODO: Declaraciones, componentes, directivas, pipes
+    AppComponent,
+  ],
+  imports: [ //TODO: Solo se importan otros modules
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  providers: [
+    CookieService,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: InjectSessionInterceptor,
+      multi: true
+    }
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
